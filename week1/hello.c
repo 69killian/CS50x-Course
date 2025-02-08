93b3f216332c622b55cdd94f89537609f2480522
Killian <martykillian7@gmail.com>
2025-02-08 10:54:05 +0100

    week 1

---
+++ week1/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    printf("hello world\n");
+}
+
+// code hello.c = create the folder with a name
+// make hello = running the compiler
+// ./hello = running the code
