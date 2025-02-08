51b9687f3b91e8445b4dd216f88e2507416314fe
Killian <martykillian7@gmail.com>
2025-02-08 10:57:52 +0100

    adding main functions

--- week1/hello.c
+++ week1/hello.c
@@ -1,10 +1,71 @@
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void) 
+int main(void)
 {
-    printf("hello world\n");
+    // define the type of variable before the name
+    string answer = get_string("What is your name? ");
+    // %s is a placeholder for strings
+    printf("hello, %s\n", answer);
 }
+
+
+
+
+
+
+
+
+
+
+// NOTES
 
 // code hello.c = create the folder with a name
 // make hello = running the compiler
 // ./hello = running the code
+// ctrl + l || clear = clear the terminal
+// ls = list
+// cd = go to file
+// cp = backup a file
+// mkdir = create directory (folder)
+// mv = move a file or rename it
+// rm = remove
+// rmdir = remove directory
+
+// get_string() function to create questions
+
+
+// TYPES
+
+// bool
+// char
+// double
+// float
+// int
+// long
+// string
+
+
+// FORMAT CODES
+
+// %c
+// %f = float
+// %i = integer
+// %li
+// %s = sring
+
+
+// CONDITIONALS
+
+/*
+
+if (x < y)
+{
+    printf("x is less than y.\n");
+} else if (x > y) {
+    printf("x is more than y.\n");
+} else if (x == y) {
+    printf("x equals y.\n");
+}
+
+*/
